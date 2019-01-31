9a19788d3aca62b805932df0176b7d1df00cef93
KeyMasterXV <vinicius.cabralxv@gmail.com>
2019-01-31 15:18:04 -0500

    Adding base entity, Closes #10.

--- pet-clinic-data/src/main/java/cabral/vinicius/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/cabral/vinicius/sfgpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package cabral.vinicius.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private PetType petType;
     private Owner owner;
