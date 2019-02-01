d9e81922c4b4f0ab2836c7e300064ad2fdd78cd6
KeyMasterXV <vinicius.cabralxv@gmail.com>
2019-02-01 16:11:30 -0500

    Impl Map based Services, Closes #6

---
+++ pet-clinic-data/src/main/java/cabral/vinicius/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package cabral.vinicius.sfgpetclinic.services.map;
+
+import cabral.vinicius.sfgpetclinic.model.Vet;
+import cabral.vinicius.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
