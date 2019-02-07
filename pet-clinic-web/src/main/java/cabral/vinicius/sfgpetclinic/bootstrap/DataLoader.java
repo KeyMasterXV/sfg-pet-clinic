c42fb94283da69e047dd1049c8c13d64cd0cad3c
KeyMasterXV <vinicius.cabralxv@gmail.com>
2019-02-07 16:25:47 -0500

    Added PetTypes to bootstrap. Closes #32.

--- pet-clinic-web/src/main/java/cabral/vinicius/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/cabral/vinicius/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package cabral.vinicius.sfgpetclinic.bootstrap;
 
 import cabral.vinicius.sfgpetclinic.model.Owner;
+import cabral.vinicius.sfgpetclinic.model.PetType;
 import cabral.vinicius.sfgpetclinic.model.Vet;
 import cabral.vinicius.sfgpetclinic.services.OwnerService;
+import cabral.vinicius.sfgpetclinic.services.PetTypeService;
 import cabral.vinicius.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
