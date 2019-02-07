44b676989f1e56f1217a818b27150fb55bbc025a
KeyMasterXV <vinicius.cabralxv@gmail.com>
2019-02-07 16:04:21 -0500

    Added specialties, added to vet. Closes #27

---
+++ pet-clinic-data/src/main/java/cabral/vinicius/sfgpetclinic/model/Specialty.java
@@ -0,0 +1,14 @@
+package cabral.vinicius.sfgpetclinic.model;
+
+public class Specialty extends BaseEntity {
+
+    private String desciption;
+
+    public String getDesciption() {
+        return desciption;
+    }
+
+    public void setDesciption(String desciption) {
+        this.desciption = desciption;
+    }
+}
