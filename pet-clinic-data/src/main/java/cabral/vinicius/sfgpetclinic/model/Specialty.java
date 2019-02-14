8818b390bbc87f64b1db0486dacf502748706b06
KeyMasterXV <vinicius.cabralxv@gmail.com>
2019-02-14 16:36:48 -0500

    Updated vet with JPA. Closes #37

--- pet-clinic-data/src/main/java/cabral/vinicius/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/cabral/vinicius/sfgpetclinic/model/Specialty.java
@@ -1,7 +1,14 @@
 package cabral.vinicius.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
 
+    @Column(name = "description")
     private String desciption;
 
     public String getDesciption() {
