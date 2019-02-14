8ac19a4cea0a42334f6eeeb696f7377ef5b8b453
KeyMasterXV <vinicius.cabralxv@gmail.com>
2019-02-14 17:26:09 -0500

    Added spring data jpa service for petType. Closes #42

---
+++ pet-clinic-data/src/main/java/cabral/vinicius/sfgpetclinic/services/springdatajpa/PetTypeSDJpa.java
@@ -0,0 +1,51 @@
+package cabral.vinicius.sfgpetclinic.services.springdatajpa;
+
+import cabral.vinicius.sfgpetclinic.model.PetType;
+import cabral.vinicius.sfgpetclinic.repositories.PetTypeRepository;
+import cabral.vinicius.sfgpetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeSDJpa implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSDJpa(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+
+        Set<PetType> petTypes = new HashSet<>();
+
+        petTypeRepository.findAll().iterator().forEachRemaining(petTypes::add);
+
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return petTypeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}
