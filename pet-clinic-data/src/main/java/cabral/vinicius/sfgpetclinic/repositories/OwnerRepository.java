c686c71d321de4c79cee67cde11b1f46842f456d
KeyMasterXV <vinicius.cabralxv@gmail.com>
2019-03-05 14:42:05 -0500

    Added functionality to add or update owners. Closes #54

--- pet-clinic-data/src/main/java/cabral/vinicius/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/cabral/vinicius/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,7 +3,12 @@ package cabral.vinicius.sfgpetclinic.repositories;
 import cabral.vinicius.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
+
 }
--- pet-clinic-data/src/main/java/cabral/vinicius/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/cabral/vinicius/sfgpetclinic/services/OwnerService.java
@@ -2,7 +2,11 @@ package cabral.vinicius.sfgpetclinic.services;
 
 import cabral.vinicius.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
